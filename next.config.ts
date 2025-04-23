02be3ea6419bbc10559925613d6f997743102bfe
tiesen243 <ttien56906@gmail.com>
2025-04-23 12:59:08 +0700

    feat: config files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+} satisfies NextConfig
 
-export default nextConfig;
+export default nextConfig
